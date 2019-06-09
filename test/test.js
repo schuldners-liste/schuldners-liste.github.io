195265be3bba9d030dae7b7a34204df104cc5a6b
dorfingerjonas <jonas.dorfinger@gmx.at>
2019-06-09 09:06:57 +0200

    finished test page

--- test/test.js
+++ test/test.js
@@ -5,9 +5,19 @@ window.addEventListener('load', () => {
     const b1 = document.getElementById('burger1');
     const b2 = document.getElementById('burger2');
     const b3 = document.getElementById('burger3');
+    const nav = document.getElementById('nav');
+    const burger = document.getElementById('navBurger');
 
     b1.classList.toggle('burger1Active');
     b2.classList.toggle('burger2Active');
     b3.classList.toggle('burger3Active');
+
+    if (nav.style.left === '0px') {
+      nav.style.left = '-70vw';
+      burger.style.left = 0;
+    } else {
+      nav.style.left = 0;
+      burger.style.left = '62vw';
+    }
   });
 });
