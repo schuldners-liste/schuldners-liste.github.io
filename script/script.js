58b71329894d6fc886319ecfe4978a30be7fd359
dorfingerjonas <jonas.dorfinger@gmx.at>
2019-06-09 19:08:30 +0200

    implementend burger and sidebar nav to main application

--- script/script.js
+++ script/script.js
@@ -22,6 +22,7 @@ window.addEventListener('load', () => {
   const signUpBtn = document.getElementById('signUpBtn');
   const signInBtn = document.getElementById('signInBtn');
   const addEntryBtn = document.getElementById('addEntryBtn');
+  const navBurger = document.getElementById('navBurger');
 
   userIcon.addEventListener('click', () => {
 
@@ -377,6 +378,26 @@ window.addEventListener('load', () => {
     } else {
       printErrorMessage(addFDB, 'Es dürfen keine Felder leer bleiben.');
       toggleAddEntryAnimation();
+    }
+  });
+
+  navBurger.addEventListener('click', () => {
+    const b1 = document.getElementById('burger1');
+    const b2 = document.getElementById('burger2');
+    const b3 = document.getElementById('burger3');
+    const nav = document.getElementById('nav');
+    const burger = document.getElementById('navBurger');
+
+    b1.classList.toggle('burger1Active');
+    b2.classList.toggle('burger2Active');
+    b3.classList.toggle('burger3Active');
+
+    if (nav.style.left === '0px') {
+      nav.style.left = '-90vw';
+      burger.style.left = 0;
+    } else {
+      nav.style.left = 0;
+      burger.style.left = '82vw';
     }
   });
 
