be842d2bc339329787e12d862055c87008819146
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-12-06 18:41:00 +0100

    added animations using js

--- script/account.js
+++ script/account.js
@@ -1,5 +1,23 @@
 window.addEventListener('load', () => {
     const continueWithGoogle = document.getElementById('continueWithGoogle');
+    const goToSignIn = document.getElementById('goToSignIn');
+    const goToSignUp = document.getElementById('goToSignUp');
+    const signUpWrapper = document.getElementById('signUpWrapper');
+    const signInWrapper = document.getElementById('signInWrapper');
+    const continueWithSignIn = document.getElementById('continueWithSignIn');
+    const continueWithSignUp = document.getElementById('continueWithSignUp');
+    const firebaseConfig = {
+        apiKey: "AIzaSyC193EbS7F1xds4fLyh4iujaF30j-XhhrY",
+        authDomain: "schuldners-liste-development.firebaseapp.com",
+        databaseURL: "https://schuldners-liste-development.firebaseio.com",
+        projectId: "schuldners-liste-development",
+        storageBucket: "schuldners-liste-development.appspot.com",
+        messagingSenderId: "1050245368401",
+        appId: "1:1050245368401:web:9968259298ddbee108c670" 
+    };
+
+    AOS.init();
+    firebase.initializeApp(firebaseConfig);
 
     continueWithGoogle.addEventListener('click', () => {
         // Activate Google Login Provider
@@ -22,5 +40,21 @@ window.addEventListener('load', () => {
         // The email of the user's account used.
         const email = error.email;
         });
+    });
+
+    continueWithSignIn.addEventListener('click', () => {
+        document.getElementById('startScreen').classList.add('fadeOut');
+        document.getElementById('signInWrapper').classList.add('fadeIn');
+        document.getElementById('signInWrapper').style.zIndex = 100;
+        continueWithGoogle.classList.add('fadeIn');
+        continueWithGoogle.style.zIndex = 100;
+    });
+
+    continueWithSignUp.addEventListener('click', () => {
+        document.getElementById('startScreen').classList.add('fadeOut');
+        document.getElementById('signUpWrapper').classList.add('fadeIn');
+        document.getElementById('signUpWrapper').style.zIndex = 100;
+        continueWithGoogle.classList.add('fadeIn');
+        continueWithGoogle.style.zIndex = 100;
     });
 });
